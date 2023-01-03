4bd1cbc456c336bb9fd1f1a513b6694c90504eae
ebveeti <56975325+ebveeti@users.noreply.github.com>
2023-01-03 05:17:43 +0200

    Add files via upload
    Theory file updated. Unnecessary math removed and explanation for the remaining updated.

--- theories/mp_tauto_and.v
+++ theories/mp_tauto_and.v
@@ -1,22 +1,5 @@
-(*This is one way to get coq understand law of excluded middle without defining it as an axiom.*)
-Theorem excluded_middle_irrefutable: forall (P:Prop), ~~(P \/ ~ P).
-  tauto.
-Qed.
-(*Author: Konstantin Weitz https://stackoverflow.com/users/607405/konstantin-weitz*)
-(*License: https://creativecommons.org/licenses/by-sa/3.0/*)
-(*No changes were made to the original code.*)
-(*https://stackoverflow.com/a/32813110*)
-
-(*Modus ponens[1] is a tautology.*)
-Theorem modus_ponens_tautology: forall P Q: Prop, ((P /\ (P -> Q)) -> Q) <-> ~~(P \/ ~ P).
-tauto.
-Qed.
-(*Discovery possibly already made earlier:*)
-(*https://math.stackexchange.com/q/3426524*)
-
-(*Modus ponens[1] is a conjunction when given a more lax metalogical interpretation. |- means turnstile.*)
-(*((P /\ (P -> Q)) |- Q)*)
-Theorem modus_ponens_and: forall P Q: Prop, ((P /\ Q) <-> (P /\ (P -> Q))).
+(*Propositional part of modus ponens is true if and only if a simple conjuction is true*)
+Theorem prop_modus_ponens_and: forall P Q: Prop, ((P /\ (P -> Q)) <-> (P /\ Q)).
 tauto.
 Qed.
 
